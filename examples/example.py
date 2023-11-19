a284a778cb0046cd65289fc7009f5cdd1ae0decf
Abhiram Shibu <abhiramshibu1998@gmail.com>
2023-11-19 14:21:46 +0530

    Examples: Basic Example to demonstate functionality
    Signed-off-by: Abhiram Shibu <abhiramshibu1998@gmail.com>

---
+++ examples/example.py
@@ -0,0 +1,11 @@
+import sys
+try:
+    from IPIdentify import IPIdentify
+except ModuleNotFoundError:
+    print("IPIdentify not found, install it by using pip install 'https://github.com/ABHIRAMSHIBU/IPIdentify/archive/refs/heads/develop.zip'")
+    sys.exit(-1)
+
+ipaddress = input("Enter IPv4 address:")
+
+enquiry = IPIdentify.IPEnquire(ipaddress)
+print("ISP of ",ipaddress," is:",IPIdentify.IPLocation.getISP(enquiry))
